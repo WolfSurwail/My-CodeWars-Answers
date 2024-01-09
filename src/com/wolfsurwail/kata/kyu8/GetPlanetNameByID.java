d8ec258172d6800bb99938b7cbf3ccb01a8f67e1
WolfSurwail <wolfsurwail@gmail.com>
2024-01-09 20:38:34 +0300

    09.01.2024

---
+++ src/com/wolfsurwail/kata/kyu8/GetPlanetNameByID.java
@@ -0,0 +1,17 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class GetPlanetNameByID {
+    static String getPlanetName(int id) {
+        return switch (id) {
+            case 1 -> "Mercury";
+            case 2 -> "Venus";
+            case 3 -> "Earth";
+            case 4 -> "Mars";
+            case 5 -> "Jupiter";
+            case 6 -> "Saturn";
+            case 7 -> "Uranus";
+            case 8 -> "Neptune";
+            default -> null;
+        };
+    }
+}
