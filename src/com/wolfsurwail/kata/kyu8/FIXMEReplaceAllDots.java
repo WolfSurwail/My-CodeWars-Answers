af982bb42e6645a112a471489afe03c025d02145
WolfSurwail <wolfsurwail@gmail.com>
2024-01-10 19:18:50 +0300

    10.01.2024

---
+++ src/com/wolfsurwail/kata/kyu8/FIXMEReplaceAllDots.java
@@ -0,0 +1,7 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class FIXMEReplaceAllDots {
+    public static String replaceDots(final String str) {
+        return str.replaceAll("\\.", "-");
+    }
+}
