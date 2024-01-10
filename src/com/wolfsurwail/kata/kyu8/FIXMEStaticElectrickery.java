e96a4f77abffa9f6b39e17975ed4ebdbf8580115
WolfSurwail <wolfsurwail@gmail.com>
2024-01-10 19:18:59 +0300

    10.01.2024

---
+++ src/com/wolfsurwail/kata/kyu8/FIXMEStaticElectrickery.java
@@ -0,0 +1,17 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class FIXMEStaticElectrickery {
+/*    private static int ONE_HUNDRED = 100;
+
+    static final Dinglemouse INST = new Dinglemouse();
+
+    private final int value;
+
+    private Dinglemouse() {
+        value = ONE_HUNDRED;
+    }
+
+    public int plus100(int n) {
+        return value + n;
+    }*/
+}
