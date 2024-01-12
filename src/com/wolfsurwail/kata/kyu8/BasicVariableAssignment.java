76b40ae870167e9b281d75d2a79615ba7645bab7
WolfSurwail <wolfsurwail@gmail.com>
2024-01-12 21:15:19 +0300

    12.01.2024

---
+++ src/com/wolfsurwail/kata/kyu8/BasicVariableAssignment.java
@@ -0,0 +1,7 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class BasicVariableAssignment {
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+}
