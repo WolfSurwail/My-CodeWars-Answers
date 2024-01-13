bdf8a547620321fae6cba3cda3cf78b5851caf3c
WolfSurwail <wolfsurwail@gmail.com>
2024-01-13 11:14:40 +0300

    13.01.2024

---
+++ src/com/wolfsurwail/kata/kyu8/MyHeadIsAtTheWrongEnd.java
@@ -0,0 +1,7 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class MyHeadIsAtTheWrongEnd {
+    public static String[] fixTheMeerkat(String[] arr) {
+        return new String[]{arr[2],arr[1],arr[0]};
+    }
+}
