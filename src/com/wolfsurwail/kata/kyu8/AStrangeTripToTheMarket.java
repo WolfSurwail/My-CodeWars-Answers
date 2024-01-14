30e8354eee23042e0a975c4f57e29208bde7b112
WolfSurwail <wolfsurwail@gmail.com>
2024-01-14 17:44:39 +0300

    14.01.2024

---
+++ src/com/wolfsurwail/kata/kyu8/AStrangeTripToTheMarket.java
@@ -0,0 +1,7 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class AStrangeTripToTheMarket {
+    public static boolean isLockNessMonster(String s){
+        return s.contains("3.50") || s.contains("three fifty") || s.contains("tree fiddy");
+    }
+}
