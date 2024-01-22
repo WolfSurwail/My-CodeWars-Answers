bd6910fa27712d7836c071177aef3394ec350567
WolfSurwail <wolfsurwail@gmail.com>
2024-01-22 21:10:40 +0300

    22.01.2024

--- src/com/wolfsurwail/kata/kyu8/a101DalmatiansSquashTheBugsNotTheDogs.java
+++ src/com/wolfsurwail/kata/kyu8/a101DalmatiansSquashTheBugsNotTheDogs.java
@@ -2,7 +2,7 @@ package com.wolfsurwail.kata.kyu8;
 
 public class a101DalmatiansSquashTheBugsNotTheDogs {
     static String howManyDalmatians(int number) {
-        return number <= 10 ? "Hardly any" :
+        return number <= 10 ? "Hardly any"  :
                 number <= 50 ? "More than a handful!" :
                         number == 101 ? "101 DALMATIANS!!!" : "Woah that's a lot of dogs!";
     }
