43be95f82844cefe95d44c6e0fe86185ccbf74b4
WolfSurwail <wolfsurwail@gmail.com>
2024-03-10 17:28:17 +0300

    10.03.2024

---
+++ src/com/wolfsurwail/kata/kyu8/GrasshopperCombineStrings.java
@@ -0,0 +1,7 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class GrasshopperCombineStrings {
+    public static String combineNames(String first,String last) {
+        return first + " " + last;
+    }
+}
