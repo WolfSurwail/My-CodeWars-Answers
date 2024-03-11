fa9c7356359d153ed65d0b2b3c17a0f14d01cde3
WolfSurwail <wolfsurwail@gmail.com>
2024-03-11 22:24:41 +0300

    11.03.2024

---
+++ src/com/wolfsurwail/kata/kyu8/PushAHashAnObjectIntoArray.java
@@ -0,0 +1,11 @@
+package com.wolfsurwail.kata.kyu8;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PushAHashAnObjectIntoArray {
+    public static List<String> push(){
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        return items;
+    }
+}
