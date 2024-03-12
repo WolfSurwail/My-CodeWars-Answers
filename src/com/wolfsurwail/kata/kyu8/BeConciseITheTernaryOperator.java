efae74472ccab472ebaa338781b0c1ea179bc047
WolfSurwail <wolfsurwail@gmail.com>
2024-03-12 18:29:42 +0300

    12.03.2024

---
+++ src/com/wolfsurwail/kata/kyu8/BeConciseITheTernaryOperator.java
@@ -0,0 +1,3 @@
+package com.wolfsurwail.kata.kyu8;
+
+public class BeConciseITheTernaryOperator { public static String describeAge(int a) {return "You're a(n) "+(a<=12?"kid":a<=17?"teenager":a<=64?"adult":"elderly");}}
