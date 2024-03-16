328d27c77bd4c5b816dcdd1dc43d7a51700f1dbd
WolfSurwail <wolfsurwail@gmail.com>
2024-03-16 15:01:43 +0300

    16.03.2024

--- src/com/wolfsurwail/kata/kyu7/CatYearsDogYears2.java
+++ src/com/wolfsurwail/kata/kyu7/CatYearsDogYears2.java
@@ -5,5 +5,10 @@ public class CatYearsDogYears2 {
         final int ownedCat = catYears < 15 ? 0 : catYears < 24 ? 1 : 2 + (catYears-24)/4;
         final int ownedDog = dogYears < 15 ? 0 : dogYears < 24 ? 1 : 2 + (dogYears-24)/5;
         return new int[]{ownedCat, ownedDog};
+
+
+
+
+        
     }
 }
