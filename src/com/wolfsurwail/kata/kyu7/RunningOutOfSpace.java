67d7039f29035a9b91376aba4d3f43a8aab008b9
WolfSurwail <wolfsurwail@gmail.com>
2024-03-17 21:13:54 +0300

    17.03.2024

---
+++ src/com/wolfsurwail/kata/kyu7/RunningOutOfSpace.java
@@ -0,0 +1,9 @@
+package com.wolfsurwail.kata.kyu7;
+
+public class RunningOutOfSpace {
+    public static String[] spacey(String[] array) {
+        for (int i=1;i<array.length;i++)
+            array[i] = array[i-1] + array[i];
+        return array;
+    }
+}
