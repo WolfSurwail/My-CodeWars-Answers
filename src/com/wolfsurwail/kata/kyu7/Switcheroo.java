95687d6f67ba6b24e46778de0d03f4fd27b1db81
WolfSurwail <wolfsurwail@gmail.com>
2024-03-19 20:36:29 +0300

    19.03.2024

--- src/com/wolfsurwail/kata/kyu7/Switcheroo.java
+++ src/com/wolfsurwail/kata/kyu7/Switcheroo.java
@@ -3,5 +3,10 @@ package com.wolfsurwail.kata.kyu7;
 public class Switcheroo {
     public static String switcheroo(String x) {
         return x.replaceAll("a","j").replaceAll("b","a").replaceAll("j","b");
+
+
+
+
+
     }
 }
