a2270b60685443b27dd0993e42b44d9374c53b4e
WolfSurwail <wolfsurwail@gmail.com>
2024-03-20 22:02:20 +0300

    20.03.2024

---
+++ src/com/wolfsurwail/kata/kyu7/Fibonacci.java
@@ -0,0 +1,13 @@
+package com.wolfsurwail.kata.kyu7;
+
+public class Fibonacci {
+    public static long fib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
