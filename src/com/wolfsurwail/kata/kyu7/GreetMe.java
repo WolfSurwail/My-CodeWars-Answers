ad7af2d58c05b9adab2e4237dcf0bf9c373911ec
WolfSurwail <wolfsurwail@gmail.com>
2024-05-03 21:20:44 +0300

    03.05.2024

--- src/com/wolfsurwail/kata/kyu7/GreetMe.java
+++ src/com/wolfsurwail/kata/kyu7/GreetMe.java
@@ -1,7 +1,7 @@
 package com.wolfsurwail.kata.kyu7;
 
 public class GreetMe {
-    public static String greet(String name){
+    public static String greet(String name) {
         return "Hello " + (name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase()) + "!";
     }
 }
