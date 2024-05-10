517bc4feaf9c68c037a2f4140f60d5b369afd19e
WolfSurwail <wolfsurwail@gmail.com>
2024-05-10 18:01:13 +0300

    10.05.2024

--- src/com/wolfsurwail/kata/kyu7/AllStarCodeChallenge22.java
+++ src/com/wolfsurwail/kata/kyu7/AllStarCodeChallenge22.java
@@ -1,6 +1,6 @@
 package com.wolfsurwail.kata.kyu7;
 
-public class AllStarCodeChallenge22{
+public class AllStarCodeChallenge22 {
     public static String toTime (int seconds) {
         int h = seconds / 3600;
         int m = (seconds - h * 3600) / 60;
