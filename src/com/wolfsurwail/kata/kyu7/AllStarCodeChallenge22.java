df93529b51daabce0c2b91f7413fc84fa56be64d
WolfSurwail <wolfsurwail@gmail.com>
2024-05-15 16:23:44 +0300

    15.05.2024

--- src/com/wolfsurwail/kata/kyu7/AllStarCodeChallenge22.java
+++ src/com/wolfsurwail/kata/kyu7/AllStarCodeChallenge22.java
@@ -4,6 +4,6 @@ public class AllStarCodeChallenge22 {
     public static String toTime (int seconds) {
         int h = seconds / 3600;
         int m = (seconds - h * 3600) / 60;
-        return h + " hour(s) and "+m+" minute(s)";
+        return h + " hour(s) and "+ m +" minute(s)";
     }
 }
